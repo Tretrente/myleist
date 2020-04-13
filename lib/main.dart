d5f7fae2e39a39b2b15217fefd07455f3eb673fa
Tretrente <63588388+Tretrente@users.noreply.github.com>
2020-04-13 13:58:48 +0200

    Prima versione con pulsante per aggiungere le liste

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:firstflutterapp/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
---
+++ lib/pages/home.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      appBar: AppBar(
+        title: Text('Mileyst'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[900],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  backgroundColor: Colors.grey,
+                  title: Center(
+                    child: Text(
+                      'New list!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[800],
+                      ),
+                    ),
+                  ),
+                  content: TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.lightBlueAccent, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      labelText: 'Insert list name',
+                    ),
+                  ),
+                  actions: <Widget>[
+                    Row(children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton.icon(
+                          onPressed: () {},
+                          label: Text('Delete list'),
+                          color: Colors.red[200],
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.red[900],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton.icon(
+                          onPressed: () {},
+                          color: Colors.green[200],
+                          icon: Icon(
+                            Icons.check_circle,
+                            color: Colors.green[600],
+                          ),
+                          label: Text('Save list'),
+                        ),
+                      ),
+                    ]),
+                  ],
+                );
+              });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black87,
+      ),
+    );
+  }
+}
